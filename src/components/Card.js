603685c7dc381e5acd2c87f318a6b70f99e01d0b
Luis H. Bajj Jr <luqven@gmail.com>
2019-10-02 16:10:31 -0400

    card-flip: 3d flip on click and fade text

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // styles
 import "../styles/Card.css";
 // components
@@ -7,22 +7,29 @@ import CardContent from "./CardContent";
 function Card(props) {
   // store the state of the card
   const [flipped, setFlipped] = useState(false);
+  const [color, setColor] = useState("transparent");
+  const [content, setContent] = useState(!flipped ? props.front : props.back);
 
   function handleClick(e) {
+    setColor("transparent");
     setFlipped(!flipped);
   }
 
-  // store the cards contents
-  const message = !flipped ? props.front : props.back;
+  useEffect(() => {
+    setTimeout(function() {
+      setColor("black");
+      setContent(!flipped ? props.front : props.back);
+    }, 500);
+  });
 
   return (
     <section className="card-container">
       <div
         onClick={e => handleClick(e)}
         className={`card ${!flipped ? "card-front" : "card-back"}`}
-      >
-        <CardContent content={message} />
-      </div>
+      ></div>
+      <CardContent color={color} content={content} />
+      <CardContent content={""} />
     </section>
   );
 }
--- src/components/CardContent.js
+++ src/components/CardContent.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function CardContent(props) {
-  return <p className="card-content">{props.content}</p>;
+  return <p className={`card-content ${props.color}`}>{props.content}</p>;
 }
